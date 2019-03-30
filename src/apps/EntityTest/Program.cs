562d38c6ed30ae646f3db2bc1452d90f1a7012ec
Marc Hernandez <marcsh@gmail.com>
2019-03-30 01:11:17 -0700

    Test Stubs

---
+++ src/apps/EntityTest/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using System.Windows;
+
+namespace EntityTest
+{
+    class Program
+    {
+        static void Main( string[] args )
+        {
+            Console.WriteLine("Hello World!");
+
+
+        }
+    }
+}
