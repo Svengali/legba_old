685616cd4c49371f0660de601fb625eb62d806d6
Marc Hernandez <marcsh@gmail.com>
2019-07-12 01:03:03 -0700

    Reformat some files

--- src/apps/Server/Server.cs
+++ src/apps/Server/Server.cs
@@ -21,79 +21,79 @@ namespace sv
 
 
 	static class Program
-{
-	struct POINTAPI
 	{
-		public Int32 x;
-		public Int32 y;
-	}
-
-	struct MSG
-	{
-		public Int32 hwmd;
-		public Int32 message;
-		public Int32 wParam;
-		public Int32 lParam;
-		public Int32 time;
-		public POINTAPI pt;
-	}
-
-	[DllImport( "user32.dll", SetLastError = true )]
-	private static extern bool PeekMessage(
-																 ref MSG lpMsg,
-													 Int32 hwnd,
-							 Int32 wMsgFilterMin,
-							 Int32 wMsgFilterMax,
-							 PeekMessageOption wRemoveMsg );
-
-	[DllImport( "user32.dll", SetLastError = true )]
-	private static extern bool TranslateMessage( ref MSG lpMsg );
-
-	[DllImport( "user32.dll", SetLastError = true )]
-	private static extern Int32 DispatchMessage( ref MSG lpMsg );
-
-	private enum PeekMessageOption
-	{
-		PM_NOREMOVE = 0,
-		PM_REMOVE
-	}
-
-	private static Int32 WM_QUIT = 0x12;
-
-
-	private static bool s_running = true;
-
-	private static void onAppExit(object sender, EventArgs e)
-	{
-		s_running = false;
-	}
-
-
-	static void Main( string[] args  )
-	{
-		sv.Main server = new sv.Main( args[0] );
-
-		server.startup();
-
-		MSG msg = new MSG();
-		while( s_running )
+		struct POINTAPI
 		{
-			while( PeekMessage( ref msg, 0, 0, 0, PeekMessageOption.PM_REMOVE ) )
-			{
-				if( msg.message == WM_QUIT )
-				{
-					break;
-				}
-
-				TranslateMessage( ref msg );
-				DispatchMessage( ref msg );
-			}
-
-			System.Threading.Thread.Sleep( 1 );
+			public Int32 x;
+			public Int32 y;
 		}
 
-		server.shutdown();
+		struct MSG
+		{
+			public Int32 hwmd;
+			public Int32 message;
+			public Int32 wParam;
+			public Int32 lParam;
+			public Int32 time;
+			public POINTAPI pt;
+		}
+
+		[DllImport( "user32.dll", SetLastError = true )]
+		private static extern bool PeekMessage(
+																	 ref MSG lpMsg,
+														 Int32 hwnd,
+								 Int32 wMsgFilterMin,
+								 Int32 wMsgFilterMax,
+								 PeekMessageOption wRemoveMsg );
+
+		[DllImport( "user32.dll", SetLastError = true )]
+		private static extern bool TranslateMessage( ref MSG lpMsg );
+
+		[DllImport( "user32.dll", SetLastError = true )]
+		private static extern Int32 DispatchMessage( ref MSG lpMsg );
+
+		private enum PeekMessageOption
+		{
+			PM_NOREMOVE = 0,
+			PM_REMOVE
+		}
+
+		private static Int32 WM_QUIT = 0x12;
+
+
+		private static bool s_running = true;
+
+		private static void onAppExit( object sender, EventArgs e )
+		{
+			s_running = false;
+		}
+
+
+		static void Main( string[] args )
+		{
+			sv.Main server = new sv.Main( args[0] );
+
+			server.startup();
+
+			MSG msg = new MSG();
+			while( s_running )
+			{
+				while( PeekMessage( ref msg, 0, 0, 0, PeekMessageOption.PM_REMOVE ) )
+				{
+					if( msg.message == WM_QUIT )
+					{
+						break;
+					}
+
+					TranslateMessage( ref msg );
+					DispatchMessage( ref msg );
+				}
+
+				System.Threading.Thread.Sleep( 1 );
+			}
+
+			server.shutdown();
+		}
 	}
-}
 
 }
--- src/libs/Services/Core/Main.cs
+++ src/libs/Services/Core/Main.cs
@@ -164,57 +164,57 @@ namespace sv
 
 
 	public enum ENodeType
-{
-	Root,
-	Leaf
-}
-
-[Serializable]
-public class SerializationTest
-{
-
-	public SerializationTest()
 	{
-		m_strings[ 0 ] = "Str 0";
-		m_strings[ 1 ] = "Str 1";
-
-		m_comList[ new lib.Token( "Token" ) ] = "Token";
-		m_comList[ new lib.Token( "Token_2" ) ] = "Token_2";
+		Root,
+		Leaf
 	}
 
-	String[] m_strings = new String[ 2 ];
+	[Serializable]
+	public class SerializationTest
+	{
 
-	Dictionary<lib.Token, string> m_comList = new Dictionary<lib.Token, string>();
-}
+		public SerializationTest()
+		{
+			m_strings[0] = "Str 0";
+			m_strings[1] = "Str 1";
 
-[Serializable]
-public struct NewService
-{
-	public string type;
-	public string name;
-	public string configPath;
-}
+			m_comList[new lib.Token( "Token" )] = "Token";
+			m_comList[new lib.Token( "Token_2" )] = "Token_2";
+		}
 
-[Serializable]
-public class ServerCfg : lib.Config
-{
-	public ENodeType node = ENodeType.Leaf;
-	public string name = "Test";
-	public string address = "0.0.0.0";
-	public int		port = 8008;
+		String[] m_strings = new String[ 2 ];
 
-	public res.Ref<svc.MachineCfg> machineCfg;
+		Dictionary<lib.Token, string> m_comList = new Dictionary<lib.Token, string>();
+	}
 
-	public NewService[] services = { new NewService() };
-	public string[] demand = { };
-}
+	[Serializable]
+	public struct NewService
+	{
+		public string type;
+		public string name;
+		public string configPath;
+	}
+
+	[Serializable]
+	public class ServerCfg : lib.Config
+	{
+		public ENodeType node = ENodeType.Leaf;
+		public string name = "Test";
+		public string address = "0.0.0.0";
+		public int    port = 8008;
+
+		public res.Ref<svc.MachineCfg> machineCfg;
+
+		public NewService[] services = { new NewService() };
+		public string[] demand = { };
+	}
 
 
 
 
-public class Main
-{
-	static public Main main;
+	public class Main
+	{
+		static public Main main;
 
 
 
@@ -244,18 +244,11 @@ public class Main
 
 
 
-
-
-
-
-
-
-
 		public lib.Clock clock { get; private set; }
 
-	public Main( string configPath )
-	{
-		main = this;
+		public Main( string configPath )
+		{
+			main = this;
 
 			s_fullscreenWin = new Window();
 			s_fullscreenWin.BackgroundColor = ConsoleColor.DarkGray;
@@ -271,175 +264,176 @@ public class Main
 
 			Process p = Process.GetCurrentProcess();
 
-		string logpath = "logs/"+Environment.MachineName+"_"+p.Id+".log";
+			string logpath = "logs/"+Environment.MachineName+"_"+p.Id+".log";
 
-		lib.Log.create( logpath );
+			lib.Log.create( logpath );
 
-		lib.Log.s_log.addDelegate( log );
+			lib.Log.s_log.addDelegate( log );
 
-		lib.Log.info( $"Command line {Environment.CommandLine}" );
-		lib.Log.info( $"Current working directory {Environment.CurrentDirectory}" );
-		lib.Log.info( $"Running as {( Environment.Is64BitProcess ? "64" : "32" )}bit on a {( Environment.Is64BitOperatingSystem ? "64" : "32" )}bit machine." );
-		lib.Log.info( $"Running on {Environment.OSVersion}" );
-		lib.Log.info( $"This machine has {Environment.ProcessorCount} processors." );
-		lib.Log.info( $"Running as {Environment.UserName}" );
+			//*
+			lib.Log.info( $"Command line {Environment.CommandLine}" );
+			lib.Log.info( $"Current working directory {Environment.CurrentDirectory}" );
+			lib.Log.info( $"Running as {( Environment.Is64BitProcess ? "64" : "32" )}bit on a {( Environment.Is64BitOperatingSystem ? "64" : "32" )}bit machine." );
+			lib.Log.info( $"Running on {Environment.OSVersion}" );
+			lib.Log.info( $"This machine has {Environment.ProcessorCount} processors." );
+			lib.Log.info( $"Running as {Environment.UserName}" );
 
-		lib.Log.info( $"Running on CLR {Environment.Version}" );
-		lib.Log.info( $"Currently given {Environment.WorkingSet} memory" );
+			lib.Log.info( $"Running on CLR {Environment.Version}" );
+			lib.Log.info( $"Currently given {Environment.WorkingSet} memory" );
+			//*/
 
 
+			/*
+			var test = new TestCalls();
+			test.runAllTests();
+			/*/
+			lib.Log.info( $"Skipping tests." );
+			//*/
 
-		/*
-		var test = new TestCalls();
-		test.runAllTests();
-		/*/
-		lib.Log.info( $"Skipping tests." );
-		//*/
-
-		res.Mgr.startup();
-		lib.Config.startup( "server_config.cfg" );
+			res.Mgr.startup();
+			lib.Config.startup( "server_config.cfg" );
 
 
 
 			lib.Util.checkAndAddDirectory( "logs" );
-		// save/static and save/dynamic are created when they dont exist in order to create the universe
-		lib.Util.checkAndAddDirectory( "save/players" );
+			// save/static and save/dynamic are created when they dont exist in order to create the universe
+			lib.Util.checkAndAddDirectory( "save/players" );
 
 
-		lib.Util.checkAndAddDirectory( "save/archive/static" );
-		lib.Util.checkAndAddDirectory( "save/archive/dynamic" );
-		lib.Util.checkAndAddDirectory( "save/archive/players" );
+			lib.Util.checkAndAddDirectory( "save/archive/static" );
+			lib.Util.checkAndAddDirectory( "save/archive/dynamic" );
+			lib.Util.checkAndAddDirectory( "save/archive/players" );
 
-		clock = new lib.Clock( 0 );
+			clock = new lib.Clock( 0 );
 
-		m_svcMgr = new svc.Mgr();
+			m_svcMgr = new svc.Mgr();
 
-		//Load configs
-		lib.Log.info( $"Loading config {configPath}" );
-		//m_cfg = lib.Config.load<ServerCfg>( configPath );
-		m_cfg = res.Mgr.lookup<ServerCfg>( configPath );
+			//Load configs
+			lib.Log.info( $"Loading config {configPath}" );
+			//m_cfg = lib.Config.load<ServerCfg>( configPath );
+			m_cfg = res.Mgr.lookup<ServerCfg>( configPath );
 
-		lib.Log.info( $"Listening on port {m_cfg.res.port}" );
-		IPEndPoint localEP = new IPEndPoint( IPAddress.Parse( m_cfg.res.address ), m_cfg.res.port ); 
+			lib.Log.info( $"Listening on port {m_cfg.res.port}" );
+			IPEndPoint localEP = new IPEndPoint( IPAddress.Parse( m_cfg.res.address ), m_cfg.res.port );
 
-		m_listener = new TcpListener( localEP );
-		m_listener.Start();
+			m_listener = new TcpListener( localEP );
+			m_listener.Start();
 
-		/*
-		foreach( NetworkInterface nic in
-				NetworkInterface.GetAllNetworkInterfaces() )
-		{
-			Console.WriteLine( nic.Name );
-			foreach( UnicastIPAddressInformation addrInfo in
-					nic.GetIPProperties().UnicastAddresses )
+			/*
+			foreach( NetworkInterface nic in
+					NetworkInterface.GetAllNetworkInterfaces() )
 			{
-				Console.WriteLine( "\t" + addrInfo.Address );
+				Console.WriteLine( nic.Name );
+				foreach( UnicastIPAddressInformation addrInfo in
+						nic.GetIPProperties().UnicastAddresses )
+				{
+					Console.WriteLine( "\t" + addrInfo.Address );
+				}
+			}
+			*/
+
+			IPEndPoint ep = (IPEndPoint)m_listener.LocalEndpoint;
+
+			string machineName = m_cfg.res.name+"/"+ep.Address.ToString() + ":" + ep.Port;
+
+			//First of all start the machine service
+
+			//This is now done 
+			//var machineCfg = res.Mgr.load<svc.MachineCfg>( m_cfg.res.machineCfg );
+
+			m_machine = new svc.Machine( new lib.Token( machineName ), m_cfg.res.machineCfg );
+			svc.Service.s_mgr.start( m_machine );
+
+			//TODO: Move these into machine startup.
+			tick();
+			Thread.Sleep( 1000 );
+
+			//Now startup all the listed services
+			foreach( var s in m_cfg.res.services )
+			{
+				var start = new svmsg.StartService( new svmsg.FilterType<svc.Machine>() );
+				start.type = s.type;
+				start.configPath = s.configPath;
+				start.name = s.name;
+
+				m_machine.send( start );
+			}
+
+
+		}
+
+		public void shutdown()
+		{
+			foreach( var c in m_cnx )
+			{
+				c.Stream.Close();
+				c.Sock.Close();
+			}
+
+			var shutdown = new svmsg.Shutdown();
+
+			m_machine.send( shutdown );
+
+			svc.Service.s_mgr.processMessages();
+		}
+
+
+
+		public void startup()
+		{
+
+			Thread thread = new Thread( new ThreadStart( this.run ) );
+			thread.Start();
+		}
+
+		public void run()
+		{
+			while( true )
+			{
+				tick();
 			}
 		}
-		*/
 
-		IPEndPoint ep = (IPEndPoint)m_listener.LocalEndpoint;
-
-		string machineName = m_cfg.res.name+"/"+ep.Address.ToString() + ":" + ep.Port;
-
-		//First of all start the machine service
-
-		//This is now done 
-		//var machineCfg = res.Mgr.load<svc.MachineCfg>( m_cfg.res.machineCfg );
-
-		m_machine = new svc.Machine( new lib.Token( machineName ), m_cfg.res.machineCfg );
-		svc.Service.s_mgr.start( m_machine );
-
-		//TODO: Move these into machine startup.
-		tick();
-		Thread.Sleep( 1000 );
-
-		//Now startup all the listed services
-		foreach( var s in m_cfg.res.services )
+		public void tick()
 		{
-			var start = new svmsg.StartService( new svmsg.FilterType<svc.Machine>() );
-			start.type = s.type;
-			start.configPath = s.configPath;
-			start.name = s.name;
+			clock.tick();
 
-			m_machine.send( start );
+			//msg.Tick tick = new msg.Tick();
+
+			//svc.Service.mgr.send( tick );
+
+			svc.Service.s_mgr.procMsg_block( 1000 );
+
+			//lib.Util.checkAndAddDirectory( "" );
+
+			if( m_listener.Pending() )
+			{
+				lib.Log.info( "Client connected" );
+
+				Socket socket = m_listener.AcceptSocket();
+
+
+				net.Conn conn = new net.Conn( socket );
+
+				m_cnx.Add( conn );
+
+				conn.recieveThread();
+			}
 		}
 
 
+		res.Ref<ServerCfg> m_cfg;
+
+		TcpListener m_listener;
+
+		List<net.Conn> m_cnx = new List<net.Conn>();
+
+		svc.Mgr m_svcMgr;
+
+		svc.Machine m_machine;
+
+
 	}
-
-	public void shutdown()
-	{
-		foreach( var c in m_cnx )
-		{
-			c.Stream.Close();
-			c.Sock.Close();
-		}
-
-		var shutdown = new svmsg.Shutdown();
-
-		m_machine.send( shutdown );
-
-		svc.Service.s_mgr.processMessages();
-	}
-
-
-
-	public void startup()
-	{
-
-		Thread thread = new Thread( new ThreadStart( this.run ) );
-		thread.Start();
-	}
-
-	public void run()
-	{
-		while( true )
-		{
-			tick();
-		}
-	}
-
-	public void tick()
-	{
-		clock.tick();
-
-		//msg.Tick tick = new msg.Tick();
-
-		//svc.Service.mgr.send( tick );
-
-		svc.Service.s_mgr.procMsg_block( 1000 );
-
-		//lib.Util.checkAndAddDirectory( "" );
-
-		if( m_listener.Pending() )
-		{
-			lib.Log.info( "Client connected" );
-
-			Socket socket = m_listener.AcceptSocket();
-
-
-			net.Conn conn = new net.Conn( socket );
-
-			m_cnx.Add( conn );
-
-			conn.recieveThread();
-		}
-	}
-
-
-	res.Ref<ServerCfg> m_cfg;
-
-	TcpListener m_listener;
-
-	List<net.Conn> m_cnx = new List<net.Conn>();
-
-	svc.Mgr m_svcMgr;
-
-	svc.Machine m_machine;
-
-
-}
 
 
 
